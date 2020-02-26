9edc216442af01f5babc6d4221cd3f8ed3cf39a2
permCoding <tt@59.ru>
2020-02-26 11:49:44 +0500

    Lection-CsvHelper

---
+++ 00 - DLL/Lection-26-02/Program-01.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using CsvHelper;
+using System.IO;
+using System.Globalization;
+using System.Collections;
+
+namespace csvlection
+{
+    public class Language
+    {
+        public string Name { get; set; }
+        public string Mask { get; set; }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string nameFile = "Languages.csv";
+
+            string line = "";
+
+            #region StreamReader
+            //StreamReader sr = new StreamReader(nameFile);
+            //while ( (line = sr.ReadLine()) != null )
+            //{
+            //    Console.WriteLine(line);
+            //}
+            //sr.Close();
+            #endregion
+
+            #region using
+            //using (StreamReader sr = new StreamReader(nameFile))
+            //{
+            //    while ( (line = sr.ReadLine()) != null )
+            //    {
+            //        Console.WriteLine(line);
+            //    }
+            //}
+            #endregion
+
+            #region CsvHelper
+            using (StreamReader sr = new StreamReader(nameFile))
+            {
+                using (CsvReader cr = new CsvReader(sr, CultureInfo.CurrentCulture))
+                {
+                    //
+                    //
+                    IEnumerable getEnum = cr.GetRecords<Language>();
+                    IEnumerator getIn = getEnum.GetEnumerator();
+                    while (getIn.MoveNext())
+                    {
+                        Console.WriteLine(((Language)getIn.Current).Name);
+                    }
+                }
+            }
+            #endregion
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
